ffa43f9bb142605184450029b03d6751597bd26c
acoder1983 <acoder1983@163.com>
2015-09-17 07:26:00 +0800

    change to query output

--- client/rssh-client.py
+++ client/rssh-client.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
+import time
 import urllib2
+import threading
 
 import util
 
@@ -22,19 +24,38 @@ def open_rssh(cmdArgs):
         response = urllib2.urlopen(startUrl, timeout=10)
 
         if response.code == 200:
-            # parse portIn and portOut
-            msg = response.read()
-            ports = msg.split(';')
+            # parse port
+            port = response.read()
+
+            cmd = ''
+            # start query output thread
+
+            def queryOutput():
+                queryUrl = util.makeQueryUrl(args['addr'], port)
+                while cmd != 'q':
+                    response = urllib2.urlopen(queryUrl, timeout=5)
+                    msg = response.read()
+                    if len(msg) == 0:
+                        time.sleep(1.0)
+                    else:
+                        # if len(cmd) > 0 and msg.find(cmd + '\n') > -1:
+                        #     msg = msg.replace(cmd + '\n', '')
+                        #     cmd = ''
+                        sys.stdout.write(msg)
+                        sys.stdout.flush()
+
+            t = threading.Thread(target=queryOutput)
+            t.start()
 
             print 'Welcome to ssh %s\n[enter \'q\' to exit]' % args['addr']
             # enter prompt
             while True:
-                cmd = raw_input('>>')
-                cmdUrl = util.makeCmdUrl(args['addr'], ports[0], ports[1], cmd)
+                cmd = raw_input()
+                cmdUrl = util.makeCmdUrl(args['addr'], port, cmd)
                 response = urllib2.urlopen(cmdUrl, timeout=5)
-                print response.read()
                 if cmd == 'q' or response.code != 200:
                     break
+
     except Exception, e:
         print 'Error: ' + str(e)
 
