cba3963a1d17f2da0f63069645ae1fed73236285
acoder1983 <acoder1983@163.com>
2015-09-24 15:17:14 +0800

    Update urls.py

--- server/rssh_site/rssh/urls.py
+++ server/rssh_site/rssh/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^start$', views.rssh_start),
     url(r'^(?P<port>[0-9]+)/exec/(?P<cmd>[\s\S]*)$', views.rssh_exec),
     url(r'^(?P<port>[0-9]+)/query$', views.rssh_query),
+    url(r'^(?P<rfile>[0-9]+)/md5$', views.rssh_file_md5),
+    url(r'^(?P<rfile>[0-9]+)/put/(?P<content>[a-f0-9]+)$', views.rssh_put_file),
 ]
